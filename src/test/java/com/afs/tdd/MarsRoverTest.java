aa624907c15fa155c0ce3fa20dace18733af6f7a
Marie Chow <chowma3@oocl.com>
2022-12-03 11:18:19 +0800

    test: add should_got_to_0_1_N_when_execute_command_given_0_0_N_and_M

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -2,9 +2,18 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class MarsRoverTest {
     @Test
-    void write_your_first_test() {
-
+    void should_got_to_0_1_N_when_execute_command_given_0_0_N_and_M() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+        // when
+        marsRover.executeCommand("M");
+        // then
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(1, marsRover.getLocationY());
+        assertEquals("N", marsRover.getDirection());
     }
 }
