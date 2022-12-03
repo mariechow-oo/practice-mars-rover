274881aa8f333f4e7017365469fcf8e3753ffb7b
Marie Chow <chowma3@oocl.com>
2022-12-03 11:18:53 +0800

    feat: implement MarsRover move N

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,4 +1,38 @@
 package com.afs.tdd;
 
 public class MarsRover {
+
+    private int locationX;
+    private int locationY;
+    private String direction;
+
+    public MarsRover(int locationX, int locationY, String direction) {
+        this.locationX = locationX;
+        this.locationY = locationY;
+        this.direction = direction;
+    }
+
+    public void executeCommand(String command) {
+        if (command.equals("M")) {
+            move();
+        }
+    }
+
+    private void move() {
+        if (direction.equals("N")) {
+            locationY++;
+        }
+    }
+
+    public int getLocationX() {
+        return locationX;
+    }
+
+    public int getLocationY() {
+        return locationY;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
 }
