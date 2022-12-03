69e64efb40f17bdb8978a7c36dd858de98400ac5
Marie Chow <chowma3@oocl.com>
2022-12-03 11:59:01 +0800

    test: add should_got_to_0_0_S_when_execute_command_given_0_0_W_and_L

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -82,4 +82,15 @@ class MarsRoverTest {
         assertEquals(0, marsRover.getLocationY());
         assertEquals("N", marsRover.getDirectionString());
     }
+    @Test
+    void should_got_to_0_0_S_when_execute_command_given_0_0_W_and_L() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, "W");
+        // when
+        marsRover.executeCommand("L");
+        // then
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("S", marsRover.getDirectionString());
+    }
 }
