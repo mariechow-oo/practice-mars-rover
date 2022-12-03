fe89bc5d0305cba17ff17e578c7379020fbfd831
Marie Chow <chowma3@oocl.com>
2022-12-03 11:29:58 +0800

    test: add should_got_to_0_negative_1_S_when_execute_command_given_0_0_S_and_M

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -16,4 +16,15 @@ class MarsRoverTest {
         assertEquals(1, marsRover.getLocationY());
         assertEquals("N", marsRover.getDirection());
     }
+    @Test
+    void should_got_to_0_negative_1_S_when_execute_command_given_0_0_S_and_M() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, "S");
+        // when
+        marsRover.executeCommand("M");
+        // then
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(-1, marsRover.getLocationY());
+        assertEquals("S", marsRover.getDirection());
+    }
 }
