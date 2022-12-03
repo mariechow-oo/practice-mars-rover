dee9a64cecaca6832538f3b77f7b6a8ba4d0394d
Marie Chow <chowma3@oocl.com>
2022-12-03 11:32:12 +0800

    test: add should_got_to_1_0_E_when_execute_command_given_0_0_E_and_M

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -27,4 +27,15 @@ class MarsRoverTest {
         assertEquals(-1, marsRover.getLocationY());
         assertEquals("S", marsRover.getDirection());
     }
+    @Test
+    void should_got_to_1_0_E_when_execute_command_given_0_0_E_and_M() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, "E");
+        // when
+        marsRover.executeCommand("M");
+        // then
+        assertEquals(1, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("E", marsRover.getDirection());
+    }
 }
